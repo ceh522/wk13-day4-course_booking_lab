a48ba839a29ff3c22c25dd973606d0d8c8345f48
Catherine Hall <catherine1992123@hotmail.com>
2020-02-27 11:23:30 +0000

    Set up models

---
+++ src/main/java/com/codeclan/example/courseBookingLab/CourseBookingLabApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.courseBookingLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseBookingLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseBookingLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/courseBookingLab/CourseBookingLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.courseBookingLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseBookingLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
